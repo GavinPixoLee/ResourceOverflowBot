7f8ae0d5225ce75f988fe4e2f130b70e0be7407a
NJ889 <58451243+NicholasJohansan@users.noreply.github.com>
2022-02-26 08:01:47 +0000

    Finish backend api and database

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from .database import database
 
 api = Blueprint("api", 'app', url_prefix="/api")
 
+@api.route('/resources')
+def resources():
+	return jsonify(database.get_resource_types()), 200
+
+@api.route('/books/topics')
+def book_topics():
+	return jsonify(database.get_book_topics()), 200
+
+@api.route('/courses/topics')
+def course_topics():
+	return jsonify(database.get_course_topics()), 200
+
+@api.route('/books/<string:topic>')
+def books(topic):
+	return jsonify(database.get_books(topic)), 200
+
+@api.route('/courses/<string:topic>')
+def courses(topic):
+	return jsonify(database.get_courses(topic)), 200
--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,33 @@
 import os
 import pymongo
 
-client = pymongo.MongoClient(os.environ['DB_URI'])
-database = client["ResourceOverflow"]
+class Database:
+	def __init__(self):
+		self.client = pymongo.MongoClient(os.environ['DB_URI'])
+		self.database = self.client["ResourceOverflow"]
 
+	def get_resource_types(self):
+		return self.database.list_collection_names()
+
+	def get_book_topics(self):
+		return self.get_distinct_topics('books')
+
+	def get_course_topics(self):
+		return self.get_dictinct_topics('courses')
+
+	def get_distinct_topics(self, resource):
+		return self.database[resource].distinct("topic")
+
+	def get_books(self, topic):
+		return self.search_resource('books', topic)
+
+	def get_courses(self, topic):
+		return self.search_resource('courses', topic)
+
+	def search_resource(self, resource, topic):
+		return list(self.database[resource].find(
+			{"topic": topic},
+			{'_id': False}
+		))
+
+database = Database()
